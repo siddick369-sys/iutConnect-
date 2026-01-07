761876f3e3d64decefa348052beafd790f6096cd
siddick369-sys <siddick369@gmail.com>
2026-01-07 14:08:10 +0100

    shhsazxqaqq

--- IutParrainage/settings.py
+++ IutParrainage/settings.py
@@ -200,7 +200,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Configuration WhiteNoise pour la compression et le cache
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
