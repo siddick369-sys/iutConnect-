40d63c8f64f2e184ec19f9b1a55f0b35d7522891
siddick369-sys <siddick369@gmail.com>
2026-01-16 20:55:44 +0100

    dhsjqsdkkaskdk

--- IutParrainage/urls.py
+++ IutParrainage/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=None)),
 
     path('k9fx-gorgore-admin-iut/', admin.site.urls),
     
